56016661dbdcbe00b08817de3d8c8bc1f1a4c192
bbarua15 <70711478+bbarua15@users.noreply.github.com>
2021-03-28 23:09:16 -0400

    updated

---
+++ Weather App/weather.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    
+    // Get Location 
+    navigator.geolocation.getCurrentPosition(success, error);
+
+    function success(pos) {
+        var lat = pos.coords.latitude;
+        var long = pos.coords.longitude;
+        weather(lat, long);
+    }
+
+    function error() {
+        console.log('There was an error');
+    }
+
+    function weather(lat, long) {
+        var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+
+        $.getJSON(URL, function(data) {
+            updateDOM(data);
+        });
+    }
+
+    function updateDOM(data) {
+        var city = data.name;
+        var temp = Math.round(data.main.temp_max);
+        var desc = data.weather[0].description;
+        var icon = data.weather[0].icon;
+        var humid = data.main.humidity;
+    	var feelsLike = Math.round(data.main.feels_like);
+
+        $('#city').html(city);
+        $('#temp').html(temp);
+        $('#desc').html(desc);
+        $('#icon').attr('src', icon);
+        $("#humid").html("Humidity: "+ humid);
+    	$("#feelsLike").html("Feels like: "+ feelsLike);
+    }
+});
+
